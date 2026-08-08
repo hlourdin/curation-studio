d00976618a789e8753c75f0b4cacecc8158ba0a2
Hervé Lourdin <herve@batch.com>
2026-08-08 17:12:20 +0200

    fix: remove plugin-basic-ssl dependency completely

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -42,12 +41,10 @@ function exportSiteApiPlugin() {
 
 export default defineConfig({
   plugins: [
-    basicSsl(),
     exportSiteApiPlugin()
   ],
   server: {
     host: '127.0.0.1',
-    https: true,
     port: 5173,
     strictPort: true
   }
